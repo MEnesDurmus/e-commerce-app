44d5d1c0243859f8e298220f695e58f3812eb556
MEnesDurmus <mnsdurmus@gmail.com>
2021-05-23 12:48:20 +0300

    Home Page Modified

--- e_commerce_app/lib/widgets/common_variables.dart
+++ e_commerce_app/lib/widgets/common_variables.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 
 //Colors
 const textColor = Color(0xff727c8e);
+const textColor2 = Color(0xff515c6f);
 const bgColor = Color(0xfff5f6f8);
 const mRedAccent = Color(0xffff6969);
 
 //Icons
-var seeAllIcon = Image.asset("assets/icons/see All.png");
+var seeAllIcon = Image.asset("assets/icons/see all.png");
 var apparelIcon = Image.asset("assets/icons/Apparel.png");
 var beautyIcon = Image.asset("assets/icons/Beauty.png");
 var shoesIcon = Image.asset("assets/icons/Shoes.png");
--- e_commerce_app/lib/widgets/home_page.dart
+++ e_commerce_app/lib/widgets/home_page.dart
@@ -11,89 +11,163 @@ class HomePage extends StatelessWidget {
     double iconSize = 100;
     return Column(
       children: [
-        Stack(
+        Container(
+          padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+          alignment: Alignment.centerLeft,
+          child: Text(
+            'Categories',
+            style: TextStyle(
+                fontSize: 30, fontWeight: FontWeight.bold, color: textColor2),
+          ),
+        ),
+        Container(
+          height: 100,
+          child: Stack(
+            children: [
+              Positioned(
+                top: 0,
+                child: CategoryIcon(apparelIcon, Text('Apperal')),
+              ),
+              Positioned(
+                  top: 0,
+                  left: 80,
+                  child: CategoryIcon(
+                    beautyIcon,
+                    Text('Beauty'),
+                  )),
+              Positioned(
+                top: 0,
+                left: 160,
+                child: CategoryIcon(shoesIcon, Text('Shoes')),
+              ),
+              Positioned(
+                top: 0,
+                left: 240,
+                child: CategoryIcon(seeAllIcon, Text('See All')),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.fromLTRB(20, 10, 0, 0),
+          alignment: Alignment.centerLeft,
+          child: Text(
+            'Latest',
+            style: TextStyle(
+                fontSize: 30, fontWeight: FontWeight.bold, color: textColor2),
+          ),
+        ),
+        Container(
+          height: 200,
+          padding: EdgeInsets.fromLTRB(10, 10, 0, 0),
+          child: ListViewWithSpace(
+            children: [
+              ColorfulBox(
+                size: tSize,
+                colors: [Colors.blue.shade300, Colors.blue.shade700],
+              ),
+              ColorfulBox(
+                size: tSize,
+                colors: [Colors.orangeAccent, Colors.deepOrangeAccent],
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class ColorfulBox extends StatelessWidget {
+  final Size size;
+  final List<Color> colors;
+  ColorfulBox({required this.size, required this.colors});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 184,
+      width: size.width - 40,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8),
+        gradient: LinearGradient(
+            colors: colors,
+            begin: Alignment.bottomLeft,
+            end: Alignment.topRight),
+      ),
+      child: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) => Row(
           children: [
-            Row(
+            Column(
               children: [
-                CategoryIcon(apparelIcon, Text('Apperal')),
-                CategoryIcon(beautyIcon, Text('Beauty')),
-                CategoryIcon(shoesIcon, Text('Shoes')),
+                Container(
+                  width: constraints.maxWidth * (5 / 9),
+                  padding: EdgeInsets.all(16),
+                  child: Text(
+                    'For all your summer clothing needs',
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                ),
+                SizedBox(height: 15),
+                Container(
+                  height: 40,
+                  padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    color: Colors.white,
+                  ),
+                  child: Row(
+                    children: [
+                      Text(
+                        'SEE MORE',
+                        style: TextStyle(fontSize: 12),
+                      ),
+                      SizedBox(width: 10),
+                      Container(
+                        height: 30,
+                        margin: EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                            color: mRedAccent, shape: BoxShape.circle),
+                        child: Container(
+                          margin: EdgeInsets.all(5),
+                          child: Icon(
+                            Icons.arrow_forward_ios,
+                            size: 16,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
               ],
             ),
           ],
         ),
-        SizedBox(height: 10),
-        Container(
-          padding: EdgeInsets.all(20),
-          child: LayoutBuilder(
-            builder: (BuildContext context, BoxConstraints constraints) =>
-                Container(
-                    height: 184,
-                    width: constraints.maxWidth,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8),
-                      gradient: LinearGradient(
-                          colors: [Colors.blue.shade300, Colors.blue.shade700],
-                          begin: Alignment.bottomLeft,
-                          end: Alignment.topRight),
-                    ),
-                    child: LayoutBuilder(
-                      builder:
-                          (BuildContext context, BoxConstraints constraints) =>
-                              Row(
-                        children: [
-                          Column(
-                            children: [
-                              Container(
-                                width: constraints.maxWidth * (5 / 9),
-                                padding: EdgeInsets.all(16),
-                                child: Text(
-                                  'For all your summer clothing needs',
-                                  style: TextStyle(
-                                      color: Colors.white, fontSize: 18),
-                                ),
-                              ),
-                              SizedBox(height: 15),
-                              Container(
-                                height: 40,
-                                padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(50),
-                                  color: Colors.white,
-                                ),
-                                child: Row(
-                                  children: [
-                                    Text(
-                                      'SEE MORE',
-                                      style: TextStyle(fontSize: 12),
-                                    ),
-                                    SizedBox(width: 10),
-                                    Container(
-                                      height: 30,
-                                      margin: EdgeInsets.all(5),
-                                      decoration: BoxDecoration(
-                                          color: mRedAccent,
-                                          shape: BoxShape.circle),
-                                      child: Container(
-                                        margin: EdgeInsets.all(5),
-                                        child: Icon(
-                                          Icons.arrow_forward_ios,
-                                          size: 16,
-                                          color: Colors.white,
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              )
-                            ],
-                          ),
-                        ],
-                      ),
-                    )),
-          ),
-        ),
-      ],
+      ),
+    );
+  }
+}
+
+class ListViewWithSpace extends StatelessWidget {
+  final List<Widget> children;
+  ListViewWithSpace({required this.children});
+
+  List<Widget> getChildren() {
+    List<Widget> result = <Widget>[];
+
+    result.add(SizedBox(width: 10));
+    for (var child in children) {
+      result.add(child);
+      result.add(SizedBox(width: 10));
+    }
+    return result;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      scrollDirection: Axis.horizontal,
+      children: getChildren(),
     );
   }
 }
